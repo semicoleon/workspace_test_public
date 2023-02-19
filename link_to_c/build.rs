b1e95b3ce9f39adb76bf9afba76aee1143cde7a5
Cole <cole@swift-mail.com>
2023-02-19 15:59:48 -0600

    Build C++ lib in build script with `cc`

--- link_to_c/build.rs
+++ link_to_c/build.rs
@@ -1,15 +1,11 @@
 use std::error::Error;
 
-fn main()-> Result<(), Box<dyn Error>> {
-    // Get the proper directory for this based on the build profile
-    let profile = std::env::var("PROFILE").unwrap();
-    let subdir = match profile.as_str() {
-        "debug" => "Debug",
-        "release" => "Release",
-        _ => panic!("Unknown cargo profile:"),
-    };
-    let cur_path = std::env::current_dir()?;
-    println!("cargo:rustc-link-search={}\\RustTestStatic\\x64\\{subdir}", cur_path.display());
+fn main() -> Result<(), Box<dyn Error>> {
+    cc::Build::new()
+        .include("RustTestStatic/RustTestStatic")
+        .file("RustTestStatic/RustTestStatic/RustTestStatic.cpp")
+        .cpp(true)
+        .compile("RustTestStatic");
 
     Ok(())
 }
